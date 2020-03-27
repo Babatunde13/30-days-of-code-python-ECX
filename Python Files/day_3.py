919d7685576eb171dc78269b29cb120e8d2e9d55
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 00:33:01 +0100

    modified day_3.py

--- Python Files/day_3.py
+++ Python Files/day_3.py
@@ -9,16 +9,19 @@ def bin_search(list_, num):
 
     list_.sort() # Ensures that the list is sorted
     no = len(list_)//2 # The index of the median element in the list
-    if num == list_[no]: # Checks if 
-        return True
-    elif num > list_[no]:
-        for val in list_[no:]:
-            if val == num:
-                return True
-            
-    elif num < list_[no]:
-        for val in list_[:no]:
-           if val == num:
-                return True 
+    while len(list_) > 0:
+        if num == list_[no]: # Checks if the number is the median value
+            return True
+        elif num > list_[no]: # Check if the number is greater than the median value
+            for val in list_[no:]: # Searches for the number in the upper half of the list.
+                if val == num:
+                    return True
+                
+        elif num < list_[no]: # Check if the number is less than the median value
+            for val in list_[:no]: # Searches for the number in the upper half of the list.
+                if val == num:
+                    return True 
+        
+        return False # Returns False if all of the condition above are not true. 
     
-    return False
+    return False # Return False if list is empty.
