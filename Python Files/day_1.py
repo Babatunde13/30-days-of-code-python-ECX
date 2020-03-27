19d47a9c826d2c5da0f9117fb710838e5331c102
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 00:18:15 +0100

    Modified day_1.py

--- Python Files/day_1.py
+++ Python Files/day_1.py
@@ -7,13 +7,13 @@ def guessing_game(no_of_guess, target):
     Returns: the positive difference between the final guess and the correct value
     '''
 
-    for num in range(no_of_guess):
-        yournum = int(input('Enter a number: '))
-        if yournum > target:
+    for num in range(no_of_guess): # iterates no_of_guesses time
+        yournum = int(input('Enter a number: ')) # gets user input
+        if yournum > target: # checks if input is greater tahn actual value
             print('Your guess is higher than the correct number')
-        elif yournum < target:
+        elif yournum < target: # checks if input is lesser
             print('Your guess is lower than the correct number')
         else:
             print('Your guess is equal to the correct number')
             break
-    return abs(target - yournum)
+    return abs(target - yournum) # Returns the positive difference between the final guess and actual value
