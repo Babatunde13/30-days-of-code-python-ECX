e4f0b96755bd5d0292ff4b3a1b3b3a392e26b8e4
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-27 21:35:17 +0100

    Updated da2.py file

---
+++ day2.py
@@ -0,0 +1,18 @@
+import random, string
+
+def password_generator(password_lenght):
+    '''
+    A function named password_generator that takes in the parameter password_lenght.
+     The function generates a password of lenght password_lenght
+
+    Returns: The generated password and also prints if the password is weak or strong.
+    '''
+
+    password = ''.join(random.choices(string.ascii_letters+str(1234567890), k=password_lenght))
+    if password_lenght < 8:
+        print('Your pass word is weak')
+    
+    else:
+        print('Your password is strong')
+
+    return password
