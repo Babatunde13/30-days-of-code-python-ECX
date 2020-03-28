c36c4e5dca5bcc3088aa9bd4a3f5ed575d638ee5
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 03:20:19 +0100

    Created day_25.py file

---
+++ Python Files/day_25.py
@@ -0,0 +1,21 @@
+def desc_triangle(a, b, c):
+    '''
+    A function named desc_triangle
+
+    Parameters: a, b and c, the lenght of each size of the triangle.
+
+    Returns: The type of triangle and the value of it's area
+    '''
+
+    # Using Hero's Formula 
+    s = (a + b + c) / 2
+    area = (s * (s-a) * (s - b) * (s - c)) ** 0.5
+
+    if a == b == c: # Checks if all sides are equal
+        return "Equilateral Triangle", area
+
+    elif a == b or a == c or b == c: # Checks if any two sides are equal
+        return "Iscocelles Triangle", area
+
+    else: # If first two are False, then triangle is a scalene triangle.
+        return "Scalene Triangle", area
