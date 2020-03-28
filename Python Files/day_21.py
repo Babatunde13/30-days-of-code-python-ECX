356adf04d8ee5059a922bfe57b4a6e85f550d08a
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 02:41:08 +0100

    Updated day_21.py file

---
+++ Python Files/day_21.py
@@ -0,0 +1,11 @@
+from math import log
+def closest_power(base_number, target_number):
+    '''
+    A function named closest power
+    Parameters: base_number and target_number
+    Returns: The minimum value of the closest power of the base number to the target number
+    '''
+    return int(log(target_number, base_number))
+
+
+
