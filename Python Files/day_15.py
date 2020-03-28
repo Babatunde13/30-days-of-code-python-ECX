16b641c918700668957cb8827da7efd8b4331091
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 15:14:01 +0100

    Updated day_15.py file

--- Python Files/day_15.py
+++ Python Files/day_15.py
@@ -1,11 +1,21 @@
 def file_reverser(file):
-    with open(file) as f:
-        file_ = f.readlines()
+    '''
+    A function named file_reverser which reverses the the lines and string in each line
 
-    file_ = [j[:-2] for j in file_][::-1]
-    lines  = [j[::-1]+'\n' for j in file_]
+    Parameter: file, a txt file which contains lyrics of song
+
+     A txt file saved as file_name.txt which reverses all lines and character in each reversed line will
+     be created after running the program
+
+     Returns: The function doesn't return anything.
+    '''
+    with open(file) as f: # Opens the file in read mode
+        file_ = f.readlines() # Saves all text in a variable file_ where each line is a list and closes the file when done.
+
+    file_ = [j[:-2] for j in file_][::-1]  # Reverses the line in the file 
+    lines  = [j[::-1]+'\n' for j in file_] # Reverses each element in each line
 
 
-    with open('file_name.txt', 'w') as f:
-        f.writelines(lines)
+    with open('file_name.txt', 'w') as f: # Opens a new file and save t as file_name.txt in write mode
+        f.writelines(lines) # Writes  the variable lines to the file
 
