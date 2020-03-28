c516ddde1f116d50aa0b1107bfd0f7d4c7b8ad66
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-28 01:52:58 +0100

    modified day_16.py

---
+++ Python Files/day_16.py
@@ -0,0 +1,13 @@
+
+ 
+
+def binary_adder(bin_1, bin_2):
+    '''
+    The function named binary_adder which takes in two parameters bin_1 and bin_2
+      The parameters are both in binary
+
+     Returns the sum of the two parameters in binary
+    '''
+
+    return int(bin(int('0b'+str(bin_1), 2) + int('0b'+str(bin_2), 2))[2:])
+
