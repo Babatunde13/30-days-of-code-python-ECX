a458e62ff57b0c553b610c486f8929aff3d1be66
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-29 19:09:41 +0100

    Modified day_8.py File

--- Python Files/day_8.py
+++ Python Files/day_8.py
@@ -1,4 +1,17 @@
 def Hangman(secret_word, N):
+    '''
+    A function named Hangman which tells the user to guess the letters in a text
+     and tells the user if they lose or win
+
+    Parameter: secret_word: The word they are to guess it's letters,
+     N the number of guesses.
+
+    Returns: win: if the user wins
+             lost: if the user loses.
+
+    @author: Babatunde Koiki
+    Created on 2020-03-29 
+    '''
     for _ in range(N):
         user_guess = input(input('Guess all the letters in the secret word: '))
         if set(secret_word.lower()) == set(user_guess.lower()):
