43c480ef8c2927c1fec8cf7d24cbe2c8b9f0f9fa
Babatunde13 <koikibabatunde14@gmail.com>
2020-03-29 19:13:08 +0100

    Modified day_21.py File

--- Python Files/day_21.py
+++ Python Files/day_21.py
@@ -7,6 +7,9 @@ def closest_power(base_number, target_number):
     Parameters: base_number and target_number
 
     Returns: The minimum value of the closest power of the base number to the target number
+    
+    @author: Babatunde Koiki
+    Created on 2020-03-24 
     '''
     return int(log(target_number, base_number))
 
