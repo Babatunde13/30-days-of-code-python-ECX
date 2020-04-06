ee853f5c54125980cfad4cd676342346ab0e876c
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-04-06 22:58:46 +0100

    Update day_9.py

---
+++ Python Files/day_9.py
@@ -0,0 +1,28 @@
+import math; 
+  
+def PowerSet(set_): 
+    '''
+    A function named PowerSet which returns the subset of a list
+    '''
+      
+    # set_size of power set of a set 
+    # with set_size n is (2**n -1) 
+    pow_set_size = int(math.pow(2, len(set_))
+    counter = 0
+    j = 0
+    powerset = []
+     
+    for counter in range(0, pow_set_size): 
+        for j in range(0, set_size): 
+              
+            # Check if jth bit in the  
+            # counter is set If set then  
+            # print jth element from set  
+            if((counter and (1 < j)) > 0): 
+                powerset.append(set(j)) 
+    
+    returns powerset
+  
+ 
+set_ = ['a', 'b', 'c']; 
+printPowerSet(set_);  
