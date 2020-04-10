a12d502a9e4ece725d1b5653ff8b11a5f31cd54a
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-04-10 12:15:24 +0100

    Update day_11.py

---
+++ Python Files/day_11.py
@@ -0,0 +1,9 @@
+def compressor(string):
+  '''
+  input strings of numbers
+  process= convert the strings to a list
+  then use a loop to count how many times such character occurs and print it as a tuple
+  '''
+ compressed = []
+ 
+ return tuple(compressed)
