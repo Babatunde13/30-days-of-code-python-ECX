bafe43310d3517432a533bf3a6bc538699c5993a
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-04-10 12:26:33 +0100

    Update day_11.py

--- Python Files/day_11.py
+++ Python Files/day_11.py
@@ -1,9 +1,15 @@
-def compressor(string):
-  '''
-  input strings of numbers
-  process= convert the strings to a list
-  then use a loop to count how many times such character occurs and print it as a tuple
-  '''
- compressed = []
- 
- return tuple(compressed)
+def compressor(tuple_):
+    """
+    A function to count the number of occurences
+    of an element in a tuple
+    """
+    list_ = list(tuple_) #convert the tuple to list
+    count_list = list(set(list_)) #convert the list to a set containing unique elements
+    count_list.sort() #sort the list in ascending order
+    tuple_list = []
+    
+    for i in range(len(count_list)):
+        count = list_.count(count_list[i])
+        tuple_list.append((int(count_list[i]), count))
+        
+    return tuple(tuple_list)
