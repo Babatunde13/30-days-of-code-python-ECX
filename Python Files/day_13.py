594440851a6d254947d1fe9a0f9c4cd3d907dfaa
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-04-10 14:09:21 +0100

    Update day_13.py

---
+++ Python Files/day_13.py
@@ -0,0 +1,16 @@
+def wrapper(para,n):
+    """
+    This function takes in a paragraph as a string and
+    aximum number of characters as an integer
+    
+    Returns the wrapped text as a string
+    
+    Created on Tue Apr  7 11:02:34 2020
+
+@author: Babatunde Koiki
+    """
+    new_text = para.split("\n")
+    final=[]
+    for each in new_text:
+        final.append('\n'.join(each[i:i+n] for i in range(0,len(each),n)))
+    return '\n'.join(final)
