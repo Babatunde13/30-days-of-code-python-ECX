c491c662dc4b37100cf260bae3cf95e9465f7a5d
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2020-04-11 14:49:50 +0100

    Update day_12.py

--- Python Files/day_12.py
+++ Python Files/day_12.py
@@ -1,18 +1,40 @@
-import re
+def Validator(email):
+
+    '''This function receives a email address as string and returns the validity'''
+    count_a = 0
+    len_email = len(email)
+    dot = email.rfind('.')
+    at = email.rfind('@')
+
+    someone = email[0:at]
+    example = email[at+1:dot]
+    top_domain = email[dot+1:]
+
+    if someone.islower()==True and someone.isascii()==True  or '.' in someone:
+        count_a+=1
+
+    if example.islower()==True and example.isalnum()==True or '-' in example :
+        count_a+=1
+    dot_ = example.find('.')
+    if '.' in example and (len(example[dot_+1:dot])==2 or len(example[dot_+1:dot])==3):
+        count_a+=1
+    if top_domain.isalpha()==True and (len(top_domain)== 2 or len(top_domain)==3):
+        count_a+=1
+
+    if count_a == 3 or count_a == 4:
+        return 'Valid Email address!'
+    else:
+        return 'Invalid Email address!'
+    
+    
+    
 
 
-def email_validator(string):
-  '''this function is to validate an email input
-  input is a string containing a said email
-  process is a regex function testing for all conditions
-  output is a valid or invalid response to whether the mail is correct or not
-  '''
-  pattern= r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-  if(re.search(pattern,email)):  
-      return "Valid Email" 
-  else:  
-      return "Invalid Email"
-  
 
-email=input('input email')
-email_validator(email)
+
+
+print(Validator("jan.eadesanya04@gmail.com"))
+print(Validator('sowunmi1994@unilag.edu.ng'))
+print(Validator('akande&fola@g-mail.com'))
+print(Validator('Amala@ewedu.village'))
+print(Validator('ecxunilag@yahoo.co.uk'))
