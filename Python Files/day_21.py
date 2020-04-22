5076937a55306249c7144fbecb9cfd2fccee356e
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-22 17:15:58 +0100

    Updated day_21.py

--- Python Files/day_21.py
+++ Python Files/day_21.py
@@ -1,17 +1,24 @@
-from math import log
-
-def closest_power(base_number, target_number):
-    '''
-    A function named closest power
-
-    Parameters: base_number and target_number
+def closest_power(base: int, target: int) -> int:
+    """
+    This function returns the closest power to raise the base whose result does not exceed target.
+    Parameter ---
+            base: the base(int) the number we want to find it's closest power to target
+            target: int
 
     Returns: The minimum value of the closest power of the base number to the target number
     
     @author: Babatunde Koiki
-    Created on 2020-03-24 
-    '''
-    return int(log(target_number, base_number))
+    Created on 2020-03-24
+    """
+    
+    a = []
+    #initial power is set at zero
+    n = 0
+    #Control the running of the loop
+    while base ** n <= target:
+        a.append(base ** n)
+        n +=1 #This increases power(n) till the required n is reached (as set in the loop)
+    
+    return len(a)-1
 
-
-
+print(closest_power(2, 63))
