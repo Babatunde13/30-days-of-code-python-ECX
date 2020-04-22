dcc1b92dac505101ff2a14ec5a827dd03e2aa98e
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-22 16:46:39 +0100

    Updated day_20.py

--- Python Files/day_20.py
+++ Python Files/day_20.py
@@ -1,7 +1,11 @@
 def closest_power(base: int, target: int) -> int:
     """
     This function returns the closest power to raise the base whose result does not exceed target.
-    Parameter
+    Parameter ---
+            base: the base(int) the number we want to find it's closest power to target
+            target: int
+
+    Return: an integer, the closest power of base to target whuch baser raise to the power of the value is < target
     """
     
     a = []
