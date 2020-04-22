8b26dd39e28bb290b8ac9e841341c34a9b0846a6
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-22 15:31:54 +0100

    Updated day_24.py

--- Python Files/day_24.py
+++ Python Files/day_24.py
@@ -1,44 +1,48 @@
-def intensive_properties(properties, units):
+def intensive_properties(properties: tuple, units :tuple) -> tuple:
     """
-    This function takes in a tuple of pressure  and  temperature  values  in SI  units 
-    (N/m^2 and  K)  and  a tuple  containing  the  desired  output  unit  as parameters 
-    then  returns  a  tuple  of the pressure  and temperature  in  the  desired  units. 
-    e.g   intensive_properties((101325, 298),(atm,  oC))  returns  (1, 25) 
-    (Valid  output
-    pressure  units:[Pa,  atm,  mmHg,  Bar]
-    output  temperature  units:[ oC, oF, oR]).
+    A function named intensive_properties which converts temperature and pressure to a desired unit.
+
+    Parameter: --------
+                properties: tuple
+                            first element is the pressure in N/m2
+                            second element is temperature  in Kelvin  
+                units: tuple
+                            first element is the unit we want to convert the pressure to, available pressure units are Pa,  atm,  mmHg and Bar 
+                            second element is the unit we want to convert the temperature to, available temperature units are  oC, oF and oR.
+    Returns ----------
+            (converter_pres, converted_temp)
+            converted_temp: the temperature in the desired unit
+            converted_pres: the pressure in the desired unit.
     """    
     try:
-        pressure, temperature = [float(i) for i in properties]
-        if pressure < 0 or temperature < 0:
+        pres, temp = [float(i) for i in properties]
+        if pres < 0 or temp < 0:
             raise TypeError
         p_unit, t_unit = units
-        #p_unit is the desired pressure unit
-        #t_unit is the desired temperature unit     
-        
+
         #Pressure conversion
         if p_unit == "Pa":
-            converted_pressure = pressure
+            converted_pres = pres
         elif p_unit == "atm":
-            converted_pressure = (pressure / 101325)
+            converted_pres = pres / 101325
         elif p_unit == "mmHg":
-            converted_pressure = ((pressure * 760) / 101325)
+            converted_pres = (pres * 760) / 101325
         elif p_unit == "Bar":
-            converted_pressure = (pressure / (10 ** 5))
+            converted_pres = pres / (10 ** 5)
         else:
             return "Enter valid units for pressure; [Pa, atm, mmHg, Bar]"
                                                 
         #Temperature conversion
         if t_unit == "°C":
-            converted_temperature = (temperature - 273)
+            converted_temp = temp - 273
         elif t_unit == "°F":
-            converted_temperature = (((temperature - 273) * (9 / 5)) + 32)
+            converted_temp = ((temp - 273) * (9 / 5)) + 32
         elif t_unit == "°R":
-            converted_temperature = ((((temperature - 273) * (9 / 5)) + 32) + 460)
+            converted_temp = (((temp - 273) * (9 / 5)) + 32) + 460
         else:
             return "Enter valid units for temperature; [°C, °F, °R]"
                     
-        return (converted_pressure,converted_temperature)    
+        return converted_pres,converted_temp    
     except ValueError:
         return "pressure and temperature values must be integers or/and floats"
     except TypeError:
