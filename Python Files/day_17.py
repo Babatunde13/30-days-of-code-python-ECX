f8650ffba26c59b9ee780b84a6334c636a641d89
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-22 16:00:44 +0100

    day_17.py

--- Python Files/day_17.py
+++ Python Files/day_17.py
@@ -1,18 +1,27 @@
-def abundant(n):
-        k = [i if n%i == 0 else 0 for i in range(1,(n//2)+1)]
-        k = sum(k)
-        if k > n:
-                return True
-        return False
+def abundant(n: int) -> bool:
+    '''
+    A function that checks if a number is abundant
+    Parameter: n: int
+    Returns: bool, True if n is abundant, otherwise False
+    '''
+    k = [i if n%i == 0 else 0 for i in range(1,(n//2)+1)]
+    k = sum(k)
+    if k > n:
+        return True
+    return False
 
 
-def sum_abundant(n):
-	
-        i, k = 1, []
-        while len(k) < n:
-                if abundant(i):
-                        k.append(i)
-                i += 1
-        return k, sum(k)
+def sum_abundant(n) -> tuple:
+    '''
+    A function that first n abundant numbers and their sum
+    Parameter: n:int --> number to end in count.
+    Returns: the first n abundant numbers in a list and their sum.
+    '''
+    i, k = 1, []
+    while len(k) < n:
+        if abundant(i):
+            k.append(i)
+        i += 1
+    return k, sum(k)
 
 print(sum_abundant(7))
